179ddff82185f14791c9cfd0588bda2db9305cba
dpsoccerdude101 <dennispavlyuk@gmail.com>
2021-10-19 17:22:45 -0400

    changed markdown to local import

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,8 @@
 import { component, useState, html, useEffect } from "haunted";
 import { getText, stringToHTML } from "../tools/utils.js";
 import marked from "marked";
+import cmsText from "../../index.md?url";
+//import workletURL from 'extra-scalloped-border/worklet.js?url'
 // ALLOWS LINE BREAKS WITH RETURN BUTTON
 marked.setOptions({
   breaks: true,
@@ -9,10 +11,9 @@ marked.setOptions({
 const Main = () => {
   const [markdownText, setMarkdownText] = useState();
   const [markupText, setMarkupText] = useState();
-  const markdownLink =
-    "https://raw.githubusercontent.com/dpsoccerdude101/dpsoccerdude101.github.io/master/index.md";
+  //const markdownLink = "https://raw.githubusercontent.com/dpsoccerdude101/dpsoccerdude101.github.io/master/index.md";
 
-  useEffect(async () => setMarkdownText(await getText(markdownLink)), []);
+  useEffect(async () => setMarkdownText(await getText(cmsText)), []);
   useEffect(() => {
     if (markdownText) {
       const markupStr = marked(markdownText);
